c99c9060710c7a73773b2dd677df14c9696d1b08
Greg Levine <levine.greg@gmail.com>
2022-08-04 16:45:34 -0400

    rules_docker-0.21.0 needs python2

--- lib/sugarcrm/multiverse/bazel.sh
+++ lib/sugarcrm/multiverse/bazel.sh
@@ -13,6 +13,19 @@ chores::sugarcrm::multiverse::bazel::install() {
         brew upgrade bazelisk
     fi
 
+    # rules_docker requires python2. python2 is not present on macOS 12. As a
+    # workaround until multiverse uses rules_docker-0.24.0, the following
+    # installs python2 and prioritizes python3 globally. Once python2 is no
+    # longer necessary, unset it with:
+    # pyenv global system; pyenv uninstall 3.10.6; pyenv uninstall 2.7.18;
+    if [[ $(pyenv versions | grep -q "3.10.6")$? -eq 0 ]]; then
+        pyenv install 3.10.6
+    fi
+    if [[ $(pyenv versions | grep -q "2.7.18")$? -eq 0 ]]; then
+        pyenv install 2.7.18
+    fi
+    pyenv global 3.10.6 2.7.18
+
     chores::sugarcrm::multiverse::bazel::test
 }
 
